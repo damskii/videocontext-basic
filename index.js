8d782550d9c9642c7edb2fb744c7cdd78f054fa6
Damian Hills <damski@gmail.com>
2019-09-24 11:03:05 +1000

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+
+/// VideoContext - import and define
+
+import VideoContext from "videocontext";
+
+var canvas = document.getElementById("canvas");
+var videoCtx = new VideoContext(canvas);
+
+//////////////////////////////////////////////////////////////////////////////
